1d9b044d32a6a6d191bed1a20f78f442e8893f72
LatishaMcCoy <98836791+LatishaMcCoy@users.noreply.github.com>
2022-03-12 19:02:11 -0500

    Add files via upload

---
+++ week04/event.js
@@ -0,0 +1,11 @@
+
+const main = document.querySelector('#main');
+
+main.addEventListener('mouseover', event => event.target.style.transform = "rotate(10deg)");
+main.addEventListener('click', event => event.target.style.opacity = "1");
+
+
+
+
+
+
